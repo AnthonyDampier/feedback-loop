f4ef3dd0bece4d8dc9b8dd386dda7e5cf7becbe8
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-02 15:04:26 -0600

    store and npm installs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+// import addition react-router-dom, react-redux, react functions
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 function App() {
 
   return (
@@ -10,6 +15,45 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Q1">Q1</Link>
+          </li>
+            <li>
+              <Link to="/Q2">Q2</Link>
+            </li>
+            <li>
+              <Link to="/Q3">Q3</Link>
+            </li>
+            <li>
+              <Link to="/Admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* routes go here */}
+        <Route exact path="/">
+          {/* <SelectYourPizza /> */}
+        </Route>
+        <Route exact path="/SelectYourPizza">
+          {/* <SelectYourPizza /> */}
+        </Route>
+
+        <Route exact path="/CustomerInfo">
+          {/* <CustomerInfo /> */}
+        </Route>
+
+        {/* Detail Page or Detail View */}
+        <Route exact path="/Checkout">
+          {/* <Checkout /> */}
+        </Route>
+        <Route exact path="/Admin">
+          {/* <Admin /> */}
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import necessary react-redux, redux, and redux-logger
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+const questions = (state = [], action) => {
+    return state;
+}
+const question = (state = [], action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        questions
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
