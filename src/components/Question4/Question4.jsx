50987c2bb05000f874e97bf4c2274e577fd29be9
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-02 16:30:23 -0600

    Question 4 (Comments) completed to base mode

---
+++ src/components/Question4/Question4.jsx
@@ -0,0 +1,55 @@
+import { useSelector, useDispatch, useStore } from "react-redux";
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+function questionFour(){
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const questionStore = useSelector((store => store.questions))
+    const answerStore = useSelector((store => store.answers))
+    const userStore = useSelector((store => store.userInfo))
+
+
+    const [answer, setAnswer] = useState(answerStore.answerFour ? answerStore.answerFour : '')
+    const [userName, setName] = useState(userStore.name ? userStore.name: '')
+
+    const handleInputChange = (event) => {
+        console.log('in handelInputChange',event.target.value);
+        setAnswer(event.target.value);
+        console.log(answerStore);
+        console.log(questionStore);
+    }
+
+    const handleNextClick = () => {
+        console.log('in handleNextClick');
+        if (answer !== ''){
+            dispatch({type: 'ADD_Q4_ANSWER', payload: answer})
+            history.push('/5');
+        } else {
+            alert('Please, fill input');
+        }
+    }
+
+    return<>
+        <h1>How well are you being supported?</h1>
+        <div>
+            <div>
+                <h3>Thanks for the help{' ' +userName}!</h3>
+                <input 
+                    type="text"
+                    onChange={handleInputChange}
+                    value={answer}
+                />
+            </div>
+            <button 
+            onClick={handleNextClick}
+            >
+                SUBMIT ✅
+            </button>
+        </div>
+    </>
+}
+
+export default questionFour;
